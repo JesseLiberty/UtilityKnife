b4cd4b3c80ccaeff9c1d5c1cc4a8abe7130c0f62
Jesse Liberty <JesseLiberty@non.se.com>
2021-02-16 06:42:49 -0500

    Add multiply method

--- UtilityKnife/Calculator/Calculator.cs
+++ UtilityKnife/Calculator/Calculator.cs
@@ -2,13 +2,18 @@
 {
     public class Calculator
     {
-        public int Add (int x, int y)
+        public int Add(int x, int y)
         {
             return x + y;
         }
-        public int Subtract (int x, int y)
+        public int Subtract(int x, int y)
         {
             return x - y;
         }
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
     }
 }
