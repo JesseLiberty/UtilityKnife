ceb54c5780c205ee5d2413325e9cb2be4f0efb28
Jesse Liberty <JesseLiberty@non.se.com>
2021-02-16 06:42:07 -0500

    Create method subtract

---
+++ UtilityKnife/Calculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace UtilityKnife.Calculator
+{
+    public class Calculator
+    {
+        public int Add (int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtract (int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
