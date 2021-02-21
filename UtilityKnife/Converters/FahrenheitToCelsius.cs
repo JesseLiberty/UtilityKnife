b1642372f5020807eb3be9e02c4b650f1f84fc11
Jesse Liberty <JesseLiberty@non.se.com>
2021-02-21 10:56:03 -0500

    Revert "Use calculator class for conversion"
    This reverts commit b5f931a1ebd4b22f6d0ee591a14fb8cec37570b5.

--- UtilityKnife/Converters/FahrenheitToCelsius.cs
+++ UtilityKnife/Converters/FahrenheitToCelsius.cs
@@ -5,11 +5,14 @@ namespace UtilityKnife.Converters
     {
         public double FahrenheitToCelsiusConverter(double fahrenheitTemp)
         {
+            double _celsius = 0.0;
+
             // (F – 32) / 5/9
+
             var step1 = Calculator.Calculator.Subtract(fahrenheitTemp, 32);
             var step2 = Calculator.Calculator.Division(step1, 5.0);
-            return Calculator.Calculator.Division(step2, 9.0);
-
+            _celsius = Calculator.Calculator.Division(step2, 9.0);
+            return _celsius;
         }
     }
 }
